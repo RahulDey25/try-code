6e24cb9c58d3592280b3e41a71fe428d31335a7b
Rahul <deyr279@gmail.com>
2024-03-26 13:21:13 +0530

    first commit

---
+++ src/components/LoginSignup/LoginSignUp.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import "./LoginSignUp.css";
+import vdoIcon from "../../assets/video-icon.png";
+
+const LoginSignUp = () => {
+  const [isSignUp, setIsSignUp] = useState(true); // State to track whether to show signup or login form
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [errors, setErrors] = useState({}); // State to track form errors
+
+  const toggleForm = () => {
+    setIsSignUp(!isSignUp); // Toggle between signup and login forms
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = {}; // Initialize new errors object
+
+    if (!email.trim()) {
+      newErrors.email = "Can't be empty"; // Set error message for email field
+    }
+    if (!password.trim()) {
+      newErrors.password = "Can't be empty"; // Set error message for password field
+    }
+    if (isSignUp && !repeatPassword.trim()) {
+      newErrors.repeatPassword = "Can't be empty"; // Set error message for repeat password field
+    }
+
+    setErrors(newErrors); // Update errors state
+
+    // Proceed with form submission if there are no errors
+    if (Object.keys(newErrors).length === 0) {
+      // Implement form submission logic here
+    }
+  };
+
+  const handleInputFocus = () => {
+    setErrors({}); // Clear errors when input fields receive focus
+  };
+
+  return (
+    <div>
+      <div className="vdo-icon">
+        <img src={vdoIcon} alt="video-icon" />
+      </div>
+
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h1>{isSignUp ? "Sign Up" : "Login"}</h1>
+          <div className={`input-box ${errors.email ? "error" : ""}`}>
+            <input
+              type="email"
+              placeholder="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onFocus={handleInputFocus}
+            />
+            {errors.email && (
+              <span className="error-message">Can't be empty</span>
+            )}
+          </div>
+
+          <div className={`input-box ${errors.password ? "error" : ""}`}>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={handleInputFocus}
+            />
+            {errors.password && (
+              <span className="error-message">Can't be empty</span>
+            )}
+          </div>
+
+          {isSignUp && (
+            <div
+              className={`input-box ${errors.repeatPassword ? "error" : ""}`}
+            >
+              <input
+                type="password"
+                placeholder="Repeat Password"
+                value={repeatPassword}
+                onChange={(e) => setRepeatPassword(e.target.value)}
+                onFocus={handleInputFocus}
+              />
+              {errors.repeatPassword && (
+                <span className="error-message">Can't be empty</span>
+              )}
+            </div>
+          )}
+
+          <button type="submit">
+            {isSignUp ? "Create an account" : "Login to your account"}
+          </button>
+          <div className="login-link">
+            <p>
+              {isSignUp
+                ? "Already have an account? "
+                : "Don't have an account? "}
+              <a href="/#" onClick={toggleForm}>
+                {isSignUp ? "Login" : "Sign Up"}
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignUp;
---
+++ src/components/MoviesCard/Movies.jsx
@@ -0,0 +1,140 @@
+// import React from "react";
+// import {
+//   Card,
+//   CardHeader,
+//   CardBody,
+//   Typography,
+// } from "@material-tailwind/react";
+// import './Movies.css'; // Importing CSS file for custom styles
+
+// export const MoviesCard = () => {
+//   return (
+//     <div className="flex flex-wrap justify-center">
+//       <Card className="mt-6 w-64 m-2"> {/* Adjusted card width and added margin */}
+//         <CardHeader color="blue-gray" className="relative h-40"> {/* Adjusted card header height */}
+//           <img className="rounded-img"
+//             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+//             alt="img"
+//           />
+//         </CardHeader>
+//         <CardBody>
+//           <Typography variant="h5" color="blue-gray" className="mb-2">
+//             2019
+//           </Typography>
+//           <Typography>
+//             The place is close to Barceloneta Beach and bus stop just 2 min by
+//             walk and near to &quot;Naviglio&quot; where you can enjoy the main
+//             night life in Barcelona.
+//           </Typography>
+//         </CardBody>
+//       </Card>
+      
+
+//       <Card className="mt-6 w-64 m-2"> {/* Adjusted card width and added margin */}
+//         <CardHeader color="blue-gray" className="relative h-40"> {/* Adjusted card header height */}
+//           <img className="rounded-img"
+//             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+//             alt="img"
+//           />
+//         </CardHeader>
+//         <CardBody>
+//           <Typography variant="h5" color="blue-gray" className="mb-2">
+//             UI/UX Review Check
+//           </Typography>
+//           <Typography>
+//             The place is close to Barceloneta Beach and bus stop just 2 min by
+//             walk and near to &quot;Naviglio&quot; where you can enjoy the main
+//             night life in Barcelona.
+//           </Typography>
+//         </CardBody>
+//       </Card>
+
+
+//       <Card className="mt-6 w-64 m-2"> {/* Adjusted card width and added margin */}
+//         <CardHeader color="blue-gray" className="relative h-40"> {/* Adjusted card header height */}
+//           <img className="rounded-img"
+//             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+//             alt="img"
+//           />
+//         </CardHeader>
+//         <CardBody>
+//           <Typography variant="h5" color="blue-gray" className="mb-2">
+//             UI/UX Review Check
+//           </Typography>
+//           <Typography>
+//             The place is close to Barceloneta Beach and bus stop just 2 min by
+//             walk and near to &quot;Naviglio&quot; where you can enjoy the main
+//             night life in Barcelona.
+//           </Typography>
+//         </CardBody>
+//       </Card>
+
+//       <Card className="mt-6 w-64 m-2"> {/* Adjusted card width and added margin */}
+//         <CardHeader color="blue-gray" className="relative h-40"> {/* Adjusted card header height */}
+//           <img className="rounded-img"
+//             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+//             alt="img"
+//           />
+//         </CardHeader>
+//         <CardBody>
+//           <Typography variant="h5" color="blue-gray" className="mb-2">
+//             UI/UX Review Check
+//           </Typography>
+//           <Typography>
+//             The place is close to Barceloneta Beach and bus stop just 2 min by
+//             walk and near to &quot;Naviglio&quot; where you can enjoy the main
+//             night life in Barcelona.
+//           </Typography>
+//         </CardBody>
+//       </Card>
+//       {/* Add more cards here if needed */}
+//     </div>
+//   );
+// };
+
+
+
+
+
+
+
+
+
+
+import React from "react";
+import demoPic from "../../assets/demoPic.jpg";
+import { MdLocalMovies } from "react-icons/md";
+import card from "./movie.module.css";
+
+const MoviesCard = () => {
+  return (
+    <>
+    <div className={card.cardContainer}>
+      <div className={card.imageContainer}>
+        <img src={demoPic} alt="img" />
+      </div>
+      <div className={card.totalContent}>
+        <div className={card.content}>
+          <div className="year"> 
+          {/*  Ei type er class er kono kaj nei pore jodi target korte hy design er jonno tai add kore rehechi. eta change kore module.css lagabo */}
+            2019<span className={card.dot}>.</span>
+          </div>
+          <div className={card.dynamicIcon}>
+            <MdLocalMovies />
+          </div>
+          <div className="icon-type">  
+          {/* etao same */}
+            <span className={card.dot}>.</span>Movie
+            <span className={card.dot}>.</span>
+          </div>
+          <div className="type">PG</div> 
+           {/* etao same */}
+        </div>
+        <div className={card.movieName}>Pushpa</div>
+      </div>
+    </div>
+</>
+  );
+};
+
+export default MoviesCard;
